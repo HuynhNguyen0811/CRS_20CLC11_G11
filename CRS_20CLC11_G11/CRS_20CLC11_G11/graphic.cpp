f6114606a80c68dc3078c9b4dc27c3a0867a1cc6
HuynhNguyen0811 <huynhnguyen08112002@gmail.com>
2021-04-15 15:43:01 +0700

    chia file graphic

--- CRS_20CLC11_G11/CRS_20CLC11_G11/graphic.cpp
+++ CRS_20CLC11_G11/CRS_20CLC11_G11/graphic.cpp
@@ -1,18 +1,6 @@
 #include "commonFunc.h"
 #include "graphic.h"
 
-void drawChoosingOutlineStu(int x, int y);
-void drawOutlineStu(int x, int y);
-void drawChoosingOutlineStaff(int x, int y);
-void drawOutlineStaff(int x, int y);
-void writeMenuLogin(int x, int y);
-void designMenuLogin(int x, int y);
-void designMenuManageCourseStaff(int x, int y);
-void designMenuManageStudentStaff(int x, int y);
-void designMenuManageSystemStaff(int x, int y);
-void designMenuStaff(int x, int y);
-void designMenuManageCourseStudent(int x, int y);
-void designMenuStudent(int x, int y);
 
 
 void drawChoosingOutlineStu(int x, int y) {
